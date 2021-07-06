9904db32703f13e12f61f4c1cc559f76373c0809
Leohawk <Leoyang158@gmail.com>
2021-07-06 00:55:18 -0500

    Fix bug in showMap.js

--- public/showPageMap.js
+++ public/showPageMap.js
@@ -165,7 +165,7 @@ map.on('load', function () {
             // redefine the end
             var end =
                 window['route' + version].features[0].geometry.coordinates[
-                    window['counter' + version] >= DistSteps ? window['counter' + count2] : window['counter' + version] + 1
+                    window['counter' + version] >= DistSteps ? window['counter' + version] : window['counter' + version] + 1
                 ];
             // Recurively enter the animate for the next version
             animate(window['counter' + version]);
@@ -201,6 +201,3 @@ map.on('load', function () {
         animate(counter1);
     })
 });
-
-   
-
